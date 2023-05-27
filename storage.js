89bbc562d9311dff985ccc20a77b9ebf0a8e4185
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-05-26 19:54:18 -0500

    Added a listener to remove the book and a new function to reduce the code

--- storage.js
+++ storage.js
@@ -20,35 +20,19 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
-/* Function that adds books to the array */
-function addBook(event) {
-  let book = new Book(bookTitle.value, bookAuthor.value, bookPages.value, bookRead.value);
-
-  if (localStorage.getItem('library')) {
-    /* Adds the new book */
-    myLibrary.push(book);
-    localStorage.setItem('library', JSON.stringify(myLibrary));
-  } else {
-    localStorage.setItem('library', JSON.stringify(myLibrary));
-  }
-
-  /* Prevents the form to be submitted */
-  event.preventDefault();
-  displayBook(book);
-}
-
-/* function that displays only the new books added */
-function displayBook(book) {
+/* Function fot the books divs */
+function generateBooks(book) {
   /* Create element */
   const div = document.createElement('div');
   const pTitle = document.createElement('p');
   const pAuthor = document.createElement('p');
   const pPages = document.createElement('p');
   const pRead = document.createElement('p');
-  const pRemove = document.createElement('p');
+  const pRemove = document.createElement('button');
 
   /* Add classes */
   div.classList.add('books');
+  pRemove.name = book.title;
 
   /* Add the text */
   pTitle.textContent = book.title;
@@ -69,42 +53,41 @@ function displayBook(book) {
   div.appendChild(pRead);
   div.appendChild(pRemove);
   booksContainer.appendChild(div);
+
+  pRemove.addEventListener('click', () => {
+    if (pRemove.name === book.title) {
+      div.remove();
+    }
+  });
+}
+
+/* Function that adds books to the array */
+function addBook(event) {
+  let book = new Book(bookTitle.value, bookAuthor.value, bookPages.value, bookRead.value);
+
+  if (localStorage.getItem('library')) {
+    /* Adds the new book */
+    myLibrary.push(book);
+    localStorage.setItem('library', JSON.stringify(myLibrary));
+  } else {
+    localStorage.setItem('library', JSON.stringify(myLibrary));
+  }
+
+  /* Prevents the form to be submitted */
+  event.preventDefault();
+  displayBook(book);
+}
+
+/* function that displays only the new books added */
+function displayBook(book) {
+  generateBooks(book);
 }
 
 /* Function that displays the books added */
 function displayBooks() {
   /* Loop throught the array */
   myLibrary.forEach((book) => {
-    /* Create element */
-    const div = document.createElement('div');
-    const pTitle = document.createElement('p');
-    const pAuthor = document.createElement('p');
-    const pPages = document.createElement('p');
-    const pRead = document.createElement('p');
-    const pRemove = document.createElement('p');
-  
-    /* Add classes */
-    div.classList.add('books');
-
-    /* Add the text */
-    pTitle.textContent = book.title;
-    pAuthor.textContent = book.author;
-    pPages.textContent = book.pages;
-    pRemove.textContent = 'REMOVE';
-    
-    if (book.read == 'on') {
-      pRead.textContent = 'Readed';
-    } else {
-      pRead.textContent = 'Not readed';
-    }
-  
-    /* Append elements */
-    div.appendChild(pTitle);
-    div.appendChild(pAuthor);
-    div.appendChild(pPages);
-    div.appendChild(pRead);
-    div.appendChild(pRemove);
-    booksContainer.appendChild(div);
+    generateBooks(book);
   });
 }
 displayBooks();
