2de0e6a953c14f9ea7528a0225880393ee95a18e
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-05-26 19:11:52 -0500

    Added a prevent default method for the submit button

--- storage.js
+++ storage.js
@@ -17,9 +17,41 @@ function Book(title, author, pages, read) {
 /* Function that adds books to the array */
 function addBook(event) {
   let book = new Book(bookTitle.value, bookAuthor.value, bookPages.value, bookRead.value);
-  myLibrary.push(book);
-  localStorage.setItem('library', JSON.stringify(myLibrary));
+
+  if (localStorage.getItem('library')) {
+    /* Reasign the library array to the localStorage library */
+    myLibrary = JSON.parse(localStorage.getItem('library'));
+    
+    /* Adds the new book */
+    myLibrary.push(book);
+    localStorage.setItem('library', JSON.stringify(myLibrary));
+  } else {
+    localStorage.setItem('library', JSON.stringify(myLibrary));
+  }
+
+  /* Prevents the form to be submitted */
   event.preventDefault();
+}
+
+/* Function that displays the books added */
+function displayBooks() {
+  /* Create element */
+  const div = document.createElement('div');
+  const pTitle = document.createElement('p');
+  const pAuthor = document.createElement('p');
+  const pPages = document.createElement('p');
+  const pRead = document.createElement('p');
+  const pRemove = document.createElement('p');
+
+  /* Add classes */
+  div.classList.add('books');
+
+  /* Append elements */
+  div.appendChild(pTitle);
+  div.appendChild(pAuthor);
+  div.appendChild(pPages);
+  div.appendChild(pRead);
+  div.appendChild(pRemove);
 }
 
 /* A click listener to the submit button */
